a2f31f53b770ecf21fd893ba28bba5390176d195
jordan <108599131+sudo-adduser-jordan@users.noreply.github.com>
2023-12-14 07:36:23 -0800

    Add: Day 6

--- AdventOfCode/AdventOfCode/Puzzle06A.cpp
+++ AdventOfCode/AdventOfCode/Puzzle06A.cpp
@@ -1,17 +1,48 @@
 #include "pch.h"
 
 #include "../Utilities/Utilities.h"
-#include "PuzzleSolvers.h"
 #include "Colors.h"
+#include "PuzzleSolvers.h"
 
 using namespace Utilities;
 
 namespace Puzzle06A
 {
+	struct Race
+	{
+		int time = 0;
+		int record = 0;
+		int wins = 0;
+	};
+
 	void PrintSolution(const std::filesystem::path& inputFile, bool shouldRender)
 	{
-		auto input = ReadAllLinesInFile(inputFile);
+		int result = 1;
 
-		std::cout << "\t\tPuzzle06A not yet solved !";
+		// Sample input.
+		// std::array<Race, 3> races = {};
+		// races[0] = { 7, 9 };
+		// races[1] = { 15, 40 };
+		// races[2] = { 30, 200 };
+
+		std::array<Race, 4> races = {};
+		races[0] = { 48, 296 };
+		races[1] = { 93, 1928 };
+		races[2] = { 85, 1236 };
+		races[3] = { 95, 1391 };
+
+		for (auto& race : races)
+		{
+			for (int index = 0; index <= race.time; index++)
+			{
+				if (race.record < ((race.time - index) * index))
+				{
+					race.wins++;
+				}
+			}
+			result *= race.wins;
+		}
+
+		std::cout << "\t\t" << GREEN_TEXT << result << RESET_COLOR;
 	}
 } // namespace Puzzle06A
--- AdventOfCode/AdventOfCode/Puzzle06B.cpp
+++ AdventOfCode/AdventOfCode/Puzzle06B.cpp
@@ -1,17 +1,39 @@
 #include "pch.h"
 
 #include "../Utilities/Utilities.h"
-#include "PuzzleSolvers.h"
 #include "Colors.h"
+#include "PuzzleSolvers.h"
 
 using namespace Utilities;
 
 namespace Puzzle06B
 {
+	using ulint = unsigned long long int;
+
+	struct Race
+	{
+		ulint time = 0;
+		ulint record = 0;
+		ulint wins = 0;
+	};
+
 	void PrintSolution(const std::filesystem::path& inputFile, bool shouldRender)
 	{
-		auto input = ReadAllLinesInFile(inputFile);
+		// Sample input.
+		// Time: 71530
+		// Distance:  940200
+		// Race race = { 71530, 940200 };
 
-		std::cout << "\t\tPuzzle06B not yet solved !";
+		Race race = { 48938595, 296192812361391 };
+
+		for (int index = 0; index <= race.time; index++)
+		{
+			if (race.record < ((race.time - index) * index))
+			{
+				race.wins++;
+			}
+		}
+
+		std::cout << "\t\t" << GREEN_TEXT << race.wins << RESET_COLOR;
 	}
 } // namespace Puzzle06B
