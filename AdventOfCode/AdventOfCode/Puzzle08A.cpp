a40680aaef6a81e8c1e4fc8ff1c929ab41d62541
jordan <108599131+sudo-adduser-jordan@users.noreply.github.com>
2023-12-17 23:27:58 -0800

    Add: Day 8

--- AdventOfCode/AdventOfCode/Puzzle08A.cpp
+++ AdventOfCode/AdventOfCode/Puzzle08A.cpp
@@ -1,17 +1,97 @@
 #include "pch.h"
 
 #include "../Utilities/Utilities.h"
-#include "PuzzleSolvers.h"
 #include "Colors.h"
+#include "PuzzleSolvers.h"
 
 using namespace Utilities;
 
 namespace Puzzle08A
 {
+
+	struct Node
+	{
+		std::string name;
+		std::string left;
+		std::string right;
+	};
+
+	const std::string START = "AAA";
+	const std::string END = "ZZZ";
+
 	void PrintSolution(const std::filesystem::path& inputFile, bool shouldRender)
 	{
 		auto input = ReadAllLinesInFile(inputFile);
+		std::vector<Node> nodes;
+		std::string intructions = input[0];
+		input.erase(input.begin(), input.begin() + 2);
+		for (auto& line : input)
+		{
+			Node node;
+			std::string token;
 
-		std::cout << "\t\tPuzzle08A not yet solved !";
+			token = line.substr(0, line.find(" "));
+			line.erase(0, line.find("(") + 1);
+			node.name = token;
+
+			token = line.substr(0, line.find(","));
+			line.erase(0, line.find(" ") + 1);
+			node.left = token;
+
+			token = line.substr(0, line.find(")"));
+			node.right = token;
+
+			nodes.push_back(node);
+		}
+
+		Node node;
+		for (const auto& searchNode : nodes)
+		{
+			if (searchNode.name == START)
+			{
+				node = searchNode;
+				break;
+			}
+		}
+
+		int result = 0;
+		while (true)
+		{
+			for (const auto& instruction : intructions)
+			{
+
+				if (node.name == END)
+				{
+					std::cout << "\t\t" << GREEN_TEXT << result << RESET_COLOR;
+					return;
+				}
+
+				if (instruction == 'L')
+				{
+					for (const auto& searchNode : nodes)
+					{
+						if (node.left == searchNode.name)
+						{
+							node = searchNode;
+							result++;
+							break;
+						}
+					}
+				}
+
+				if (instruction == 'R')
+				{
+					for (const auto& searchNode : nodes)
+					{
+						if (node.right == searchNode.name)
+						{
+							node = searchNode;
+							result++;
+							break;
+						}
+					}
+				}
+			}
+		}
 	}
 } // namespace Puzzle08A
--- AdventOfCode/AdventOfCode/Puzzle08B.cpp
+++ AdventOfCode/AdventOfCode/Puzzle08B.cpp
@@ -1,17 +1,121 @@
 #include "pch.h"
 
 #include "../Utilities/Utilities.h"
-#include "PuzzleSolvers.h"
 #include "Colors.h"
+#include "PuzzleSolvers.h"
 
 using namespace Utilities;
 
 namespace Puzzle08B
 {
+
+	struct Node
+	{
+		std::string name;
+		std::string left;
+		std::string right;
+	};
+
+	const std::string START = "AAA";
+	const std::string END = "ZZZ";
+	typedef long long int llint;
+
+	static llint GCD(llint a, llint b)
+	{
+		if (b == 0)
+		{
+			return a;
+		}
+		return GCD(b, a % b);
+	}
+
+	static llint LCM(std::vector<llint> vector, llint size)
+	{
+		llint ans = vector[0];
+		for (llint i = 1; i < size; i++)
+		{
+			ans = (((vector[i] * ans)) / (GCD(vector[i], ans)));
+		}
+		return ans;
+	}
+
 	void PrintSolution(const std::filesystem::path& inputFile, bool shouldRender)
 	{
 		auto input = ReadAllLinesInFile(inputFile);
+		std::vector<Node> nodes;
+		std::vector<Node> startNodes;
+		std::string intructions = input[0];
+		input.erase(input.begin(), input.begin() + 2);
+		for (auto& line : input)
+		{
+			Node node;
+			std::string token;
 
-		std::cout << "\t\tPuzzle08B not yet solved !";
+			token = line.substr(0, line.find(" "));
+			line.erase(0, line.find("(") + 1);
+			node.name = token;
+
+			token = line.substr(0, line.find(","));
+			line.erase(0, line.find(" ") + 1);
+			node.left = token;
+
+			token = line.substr(0, line.find(")"));
+			node.right = token;
+
+			nodes.push_back(node);
+			if (node.name[2] == 'A')
+			{
+				startNodes.push_back(node);
+			}
+		}
+
+		std::vector<llint> results;
+		for (auto& startingNode : startNodes)
+		{
+			llint result = 0;
+			bool isFound = false;
+			while (!isFound)
+			{
+				for (const auto& instruction : intructions)
+				{
+					result++;
+					if (instruction == 'L')
+					{
+						auto cmp = [startingNode](Node node) { return startingNode.left == node.name; };
+						auto it = std::find_if(nodes.begin(), nodes.end(), cmp);
+
+						if (it != nodes.end())
+						{
+							startingNode = *it;
+						}
+
+						if (startingNode.name[2] == 'Z')
+						{
+							results.push_back(result);
+							isFound = true;
+						}
+					}
+
+					if (instruction == 'R')
+					{
+						auto cmp = [startingNode](Node node) { return startingNode.right == node.name; };
+						auto it = std::find_if(nodes.begin(), nodes.end(), cmp);
+						if (it != nodes.end())
+						{
+							startingNode = *it;
+						}
+
+						if (startingNode.name[2] == 'Z')
+						{
+							results.push_back(result);
+							isFound = true;
+						}
+					}
+				}
+			}
+		}
+
+		std::cout << "\t\t" << GREEN_TEXT << LCM(results, results.size()) << RESET_COLOR;
 	}
+
 } // namespace Puzzle08B
